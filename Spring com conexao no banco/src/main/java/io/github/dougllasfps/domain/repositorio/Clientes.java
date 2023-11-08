d30ebef5c601d1d580b7456ca72bcd6abbc8ba07
Tiago Souza <tgsouza86@gmail.com>
2023-11-08 13:10:01 -0300

    trabalhando com Query

--- Spring com conexao no banco/src/main/java/io/github/dougllasfps/domain/repositorio/Clientes.java
+++ Spring com conexao no banco/src/main/java/io/github/dougllasfps/domain/repositorio/Clientes.java
@@ -4,14 +4,20 @@ import io.github.dougllasfps.domain.entity.Cliente;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface Clientes extends JpaRepository<Cliente, Integer> {
-
-    //List<Cliente> findByNomeLike(String nome);
-   boolean existsByNome(String nome);
+ @Query(value = "select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    List<Cliente> encontrarPorNome(@Param("nome") String nome);
+ @Query(" delete from Cliente c where c.nome =:nome ")
+ @Modifying
+ void deleteByNome(String nome);
+ boolean existsByNome(String nome);
 
 
 
